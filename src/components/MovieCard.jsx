5ef7465517ea20e3e8b81a439d8e3df304ad7899
Gerd Braun <webmaster@your-d-sign.de>
2024-10-17 19:09:28 +0200

    moved MoviePreview to src

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,24 @@
-import { Link } from 'react-router-dom'
-import Rating from './Rating'
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
 
-const MovieCard = ({movie}) => {
+const MovieCard = ({ movie }) => {
+  const imgPath = movie.poster_path
+    ? `https://media.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`
+    : "https://placehold.co/220x330?text=no%20image%20available";
   return (
     <>
-    <Link to={`/preview/${movie.id}`}>
-    <img className="rounded-t-lg hover:opacity-50" src={`https://media.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`} />
-    </Link>
-    <Rating percent={movie.vote_average}/>
-    <div><h3 className="p-2 text-center text-sm">{movie.title}</h3></div>
+      <Link to={`/preview/${movie.id}`}>
+        <img
+          className="rounded-t-lg hover:opacity-50"
+          src={imgPath}
+        />
+      </Link>
+      <Rating percent={movie.vote_average} />
+      <div>
+        <h3 className="p-2 text-center text-sm">{movie.title}</h3>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
