4176ecf9919a8bea6dfe705a16788a55ecbb38bf
Gerd Braun <webmaster@your-d-sign.de>
2024-10-17 20:16:10 +0200

    work in progress

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,16 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav>
-      <div>
-        <Link to="about">About Us</Link>
-      </div>
+      <ul className="flex gap-2 items-center">
+        <li>
+          <Link to="/" className="text-white m-2">Home</Link>
+        </li>
+        <li>
+          <Link to="about" className="text-white m-2">About Us</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import MovieList from "./MovieList";
 import Header from "./Header";
 import Footer from "./Footer";
 
@@ -8,7 +7,6 @@ const Page = () => {
     <>
       <Header />
       <main>
-        <MovieList />
         <Outlet />
       </main>
       <Footer />
